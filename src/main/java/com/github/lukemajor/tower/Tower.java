be3502ae110589d84b815abfa9d07d4b49955fa5
Luke Major <luke.major@my.wheaton.edu>
2020-04-22 14:31:01 -0500

    new initial commit

---
+++ src/main/java/com/github/lukemajor/tower/Tower.java
@@ -0,0 +1,10 @@
+package com.github.lukemajor.tower;
+
+public class Tower {
+    public static void main(String[] args) {
+        //create 10 levels with 10 rooms
+        //create player
+        System.out.println("Nooooooo!");
+    }
+
+}
